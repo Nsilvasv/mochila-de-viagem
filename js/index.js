fbb3459891fdbbf63db7a79c27f4094ac4c53e96
Nathan <nathansilva406@gmail.com>
2023-07-22 13:26:22 -0300

    verifica se elemento

--- js/index.js
+++ js/index.js
@@ -1,28 +1,50 @@
 const form = document.querySelector("#form")
 const ul = document.querySelector(".lista")
+const listaItens = JSON.parse(localStorage.getItem("itens")) || []
+
+listaItens.forEach(element => {
+    createElement(element)
+
+});
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const qtd = form.quantidade.value;
-    const nome = form.nome.value;
+    const itens =  {"nome": form.nome.value, "quantidade": form.quantidade.value } 
 
-    createElement(nome,qtd)
+    const existe = listaItens.find(n => n.nome === itens.nome )
+    
+    if (existe) {
+        console.log("existe");
+        
+    } else {
+        console.log("não");
+    }
+    
+    listaItens.push(itens)
+    
+    localStorage.setItem("itens",JSON.stringify(listaItens))
 
+    createElement(itens)
+
+    form.reset()
 } )
 
-function createElement(nome, qtd) {
+function createElement(itens) {
  
     const li =  document.createElement("li")
     li.classList.add("item")
 
     const strong = document.createElement("strong")
-    strong.innerHTML = qtd
+    strong.innerHTML = itens.quantidade
     li.appendChild(strong)
 
-    li.innerHTML += nome
+    li.innerHTML += itens.nome
 
     ul.appendChild(li) 
-    
+
+
 }
+
+
 
